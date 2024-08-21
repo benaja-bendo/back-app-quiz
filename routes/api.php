eeca36067ce0199ea8144da9f7d47cbd83b33f6f
benji_mac <benaja.bendo02@gmail.com>
2024-08-21 18:19:00 +0200

    fix test

--- routes/api.php
+++ routes/api.php
@@ -43,13 +43,24 @@ Route::group(['prefix' => 'v1'], function () {
         "https://eduscol.education.fr/rid271/toute-l-actualite-du-site.rss",
     ];
 
-    Route::get('veille/sites', function () use ($allSite) {
+    /*Route::get('veille/sites', function () use ($allSite) {
         return response()->json([
             'data' => $allSite,
         ], 200);
-    });
-    Route::get('/veille', function (Request $request) use ($allSite) {
-
+    });*/
+   /* Route::get('/veille', function (Request $request) use ($allSite) {
+        function getDateIA(string $prompt): ?string
+        {
+            $yourApiKey = env('OPENAI_API_KEY');
+            $client = OpenAI::client($yourApiKey);
+            $result = $client->chat()->create([
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    ['role' => 'user', 'content' => $prompt],
+                ],
+            ]);
+            return $result->choices[0]->message->content;
+        }
         $params = explode(",", $request->input('sites', ''));
 
         $array_unic_format = [];
@@ -92,7 +103,7 @@ Route::group(['prefix' => 'v1'], function () {
         return response()->json([
             'data' => $data,
         ], 200);
-    });
+    });*/
 
     Route::get('/documentation/json', function (Request $request) {
         $openapi = \OpenApi\Generator::scan(['../app']);
@@ -102,39 +113,3 @@ Route::group(['prefix' => 'v1'], function () {
     })->name('documentation.json');
 
 });
-
-function getDateIA(string $prompt): ?string
-{
-    $yourApiKey = env('OPENAI_API_KEY');
-    $client = OpenAI::client($yourApiKey);
-    $result = $client->chat()->create([
-        'model' => 'gpt-3.5-turbo',
-        'messages' => [
-            ['role' => 'user', 'content' => $prompt],
-        ],
-    ]);
-    return $result->choices[0]->message->content;
-}
-
-//    Méthode 1
-//    $result = Process::run('node ../app.js');
-//    dd($result->output());
-
-//    Méthode 2
-//    $client = \Symfony\Component\Panther\Client::createChromeClient();
-//    $crawler = $client->request('GET', 'https://www.lesnumeriques.com/souris/logi tech-g309-lightspeed-p75280/test.html');
-//
-//    dd($crawler->html());
-//
-//        // TODO ... Interactions avec la page (clics, etc.) ...
-//
-//    $articles = $crawler->filter('article.votre_classe_article')->each(function ($node) {
-//        // TODO... Extraction des données ...
-//    });
-
-//    Méthode 3
-//    $f = Vedmant\FeedReader\Facades\FeedReader::read('https://www.linforme.com/rss/all_headline.xml'); // ok
-//    $f = Vedmant\FeedReader\Facades\FeedReader::read('https://news.google.com/news/rss');
-//    $f = Vedmant\FeedReader\Facades\FeedReader::read('https://www.frandroid.com/tag/flux-rss'); // ok
-//    $f = Vedmant\FeedReader\Facades\FeedReader::read('https://www.tech2tech.fr/tag/flux-rss/'); // ok
-//        $f = Vedmant\FeedReader\Facades\FeedReader::read('https://www.01net.com/info/flux-rss/'); // ok
